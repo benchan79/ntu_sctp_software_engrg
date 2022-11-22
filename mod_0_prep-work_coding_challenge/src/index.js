7c033ad90db4fc8de7a1b5de396f22eaa65bf383
Ben Chan <benchan79@gmail.com>
2022-11-23 02:32:25 +0800

    Add solution

--- mod_0_prep-work_coding_challenge/src/index.js
+++ mod_0_prep-work_coding_challenge/src/index.js
@@ -1,2 +1,19 @@
 const road = document.querySelector("#road");
 road.width = 1200;
+
+const car = document.querySelector(".absolute");
+
+// const car_elem = document.getElementById("car");
+
+let modifier = 10;
+
+document.addEventListener('keydown', (e) => {
+    const car_pos = parseFloat(getComputedStyle(car).left);
+
+    switch (e.code) {
+        case 'KeyA': car.style.left = `${car_pos - modifier}px`;
+        break;
+        case 'KeyD': car.style.left = `${car_pos + modifier}px`;
+        break;
+    }
+});
