9e85521f960465d6f435f6467d91a574a2d2c4b0
Ben Chan <benchan79@gmail.com>
2022-11-23 09:10:43 +0800

    Restrict movement inside road img

--- mod_0_prep-work_coding_challenge/src/index.js
+++ mod_0_prep-work_coding_challenge/src/index.js
@@ -1,19 +1,27 @@
 const road = document.querySelector("#road");
 road.width = 1200;
 
-const car = document.querySelector(".absolute");
-
 // const car_elem = document.getElementById("car");
+const car = document.querySelector(".absolute");
+car.width = 256;
 
-let modifier = 10;
+let modifier = 10
 
 document.addEventListener('keydown', (e) => {
     const car_pos = parseFloat(getComputedStyle(car).left);
-
+    // console.log(car_pos)
     switch (e.code) {
-        case 'KeyA': car.style.left = `${car_pos - modifier}px`;
-        break;
-        case 'KeyD': car.style.left = `${car_pos + modifier}px`;
-        break;
+        case 'KeyA':
+            if (car_pos > 10) {
+                car.style.left = `${car_pos - modifier}px`;
+            }
+            break;
+        case 'KeyD':
+            if (car_pos < (road.width-car.width)) {
+                car.style.left = `${car_pos + modifier}px`;
+            }
+            break;
+        default:
+        // do nothing
     }
 });
