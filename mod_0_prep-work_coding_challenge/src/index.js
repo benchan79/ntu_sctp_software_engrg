2af70621650f4737147c0f15bdde8c82b3df32f7
Ben Chan <benchan79@gmail.com>
2022-11-24 09:14:50 +0800

    Apply prettify

--- mod_0_prep-work_coding_challenge/src/index.js
+++ mod_0_prep-work_coding_challenge/src/index.js
@@ -4,22 +4,22 @@ road.width = 1200;
 const car = document.querySelector("#car");
 car.width = 256;
 
-let modifier = 10
+let modifier = 10;
 
-document.addEventListener('keydown', (e) => {
-    const car_pos = parseFloat(getComputedStyle(car).left);
-    switch (e.code) {
-        case 'KeyA':
-            if (car_pos > 10) {
-                car.style.left = `${car_pos - modifier}px`;
-            }
-            break;
-        case 'KeyD':
-            if (car_pos < (road.width-car.width)) {
-                car.style.left = `${car_pos + modifier}px`;
-            }
-            break;
-        default:
-        // do nothing
-    }
+document.addEventListener("keydown", (e) => {
+  const car_pos = parseFloat(getComputedStyle(car).left);
+  switch (e.code) {
+    case "KeyA":
+      if (car_pos > 10) {
+        car.style.left = `${car_pos - modifier}px`;
+      }
+      break;
+    case "KeyD":
+      if (car_pos < road.width - car.width) {
+        car.style.left = `${car_pos + modifier}px`;
+      }
+      break;
+    default:
+    // do nothing
+  }
 });
